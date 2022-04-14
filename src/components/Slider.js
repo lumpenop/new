7798d5523440043f48fead8e9e10d3cc443f131d
lumpenop <swk5373@gmail.com>
2022-04-14 17:18:07 +0900

    Feat: Slider.js 생성 but Slide Bar를 직접 구현하여 마우스 클릭 시에만 바 길이가 변경됨 -> input type:range를 이용하여 새로 구현하는 방법 고려해봐야 함

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,19 +1,114 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import styled from 'styled-components';
 
 const Slider = () =>{
+
+    const [barWidth, setBarWidth] = useState(30);
+    const widthRef = useRef();
+
+    const sliderClick =(e)=>{
+        console.log(e.target);
+        const mouseX = e.nativeEvent.offsetX;
+        const sliderWidth = widthRef.current.offsetWidth;
+        const sliderBarPer = parseInt(Math.round(mouseX/sliderWidth*100));
+        console.log(sliderBarPer);
+        setBarWidth(sliderBarPer);
+    }
+
     return(
         <>
-            <SlideContainer>
-                
-            </SlideContainer>
+            <SliderContainer ref={widthRef}>
+                <StateBox>
+                    <StateInput value={barWidth} /> %
+                </StateBox>
+                <SliderBox>
+                    <SliderBarBase 
+                        onMouseDown={e=>sliderClick(e)}
+                    >
+                        <SliderBar barWidth={barWidth}>
+                            <SliderSwitch>
+                                <SliderCircle />
+                            </SliderSwitch>
+                        </SliderBar>
+                    </SliderBarBase>
+                </SliderBox>
+            </SliderContainer>
         </>
     )
 }
 
-const SlideContainer = styled.div`
+const SliderContainer = styled.div`
     width: 50%;
     margin: 0 auto;
+`
+
+const StateInput = styled.input.attrs({
+    readOnly: true,
+})`
+    display: inline-block;
+    width: 80%;
+    height: 30px;
+    border: none;
+    outline: none;
+    background-color: #fafafa;
+    text-align: right;
+`
+
+const StateBox = styled.div`
+    width: 100%;
+    height: 50px;
+    background-color: #fafafa;
+    border: 1px solid #aaa;
+    border-radius: 4px;
+    line-height: 50px;
+`
+
+const SliderBox = styled.div`
+    display: block;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+`
+
+const SliderBarBase = styled.div`
+    position: relative;
+    display: inline-block;
+    width: 100%;
+    height: 5px;
+    background-color: #ececec;
+    padding: 2px 0;
+`
+
+const SliderBar = styled.div`
+    position: absolute;
+    width: ${props => props.barWidth}%;
+    height:5px;
+    background-color: skyblue;
+
+`
+
+const SliderSwitch = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(50%, -8.5px);
+    width: 20px;
+    height: 20px;
+    border-radius: 15px;
+    background-color: #fafafa;
+    box-shadow: 1px 2px 4px #dadada;
+    pointer-events: none; 
+`
+
+const SliderCircle = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 15px;
+    height: 15px;
+    border-radius: 15px;
+    background-color: skyblue;
 `
 
 
